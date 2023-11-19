f37977f550b25959f3f0d049559aa627b654ad6e
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-19 12:26:49 +0000

    implement directional and sending message keys

--- input.js
+++ input.js
@@ -1,10 +1,30 @@
+let connection; 
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   const handleUserInput = function(key) {
     // check for ctrl+c
-    if (key === '\u0003') {
+    if (key === "\u0003") {
       process.exit();
+    }
+
+    // directional keys
+    if (key === "w") {
+      connection.write("Move: up");
+    }
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
+    }
+    if(key === " ") {
+      connection.write("Say: Noob");
     }
   };
 
