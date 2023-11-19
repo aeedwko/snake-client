a3967739c10608a1b3ffa1d6fc18135fb09aba15
Edwin Ko <edwin.mk.ko@gmail.com>
2023-11-19 11:53:30 +0000

    setup stdin in play.js

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 const { connect } = require('./client.js');
 
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  const handleUserInput = function(key) {
+    // check for ctrl+c
+    // if (key === '\u0003') {
+    //   process.exit();
+    // }
+  };
+
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
 console.log("Connecting ...");
 connect();
 
